69503cd132b7a30b15a491f4b2e9297c0a8a1399
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-03-29 19:30:33 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,8 @@
-const params = window.location.search.replace("?", "").split("&").map((i) => ({ name: i.split("=")[0], value: i.split("=")[1].replaceAll("%20", " ") }));
+const params = window.location.search.replace("?", "").split("&").map((i) => ({ name: i.split("=")[0], value: i.split("=")[1]?.replaceAll("%20", " ") }));
 const paths = window.location.pathname.split("/");
 (
 	async () => {
 		const articles = await fetch("./data/preview-articles.json").then((response) => response.json());
-		for (const article of articles)
-			document.getElementById("articles").innerHTML = `<li class="article-list-item "><a href="/changelog?version=${article.version}&beta=true" class="article-list-link">${article.article.title}</a></li>`;
+		document.getElementById("articles").innerHTML = articles.map((article) => `<li class="article-list-item "><a href="./changelog?version=${article.version}&beta=true" class="article-list-link">${article.article.title}</a></li>`);
 	}
 )();
