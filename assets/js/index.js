93cc1378d1b9b9b54a9e4883da6ff4b55c786075
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-03-29 19:01:16 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,19 @@
-const params = window.location.search.replace("?", "").split("&").map((i) => ({ name: i.split("=")[0], value: i.split("=")[1] }));
+const params = window.location.search.replace("?", "").split("&").map((i) => ({ name: i.split("=")[0], value: i.split("=")[1].replaceAll("%20", " ") }));
 const paths = window.location.pathname.split("/");
 (
 	async () => {
-		const articles = await fetch("./data/preview-articles.json").then((response) => response.json());
+		const articles = await fetch(
+			(
+				params.find((p) => p.name == "beta")?.value || "false" == true
+					? "./data/preview-articles.json"
+					: "./data/stable-articles.json"
+			),
+		).then((response) => response.json());
 
 		const article = articles.find((a) => a.version == params.find((p) => p.name == "version")?.value);
 		if (article) {
 			document.getElementById("articleTitle").innerText = article.article.title;
-			document.getElementById("articleBody").innerHTML = article.article.body;
+			document.getElementById("articleBody").innerHTML = article.article.body.replaceAll("font-family: 'Segoe UI',sans-serif;", "font-family: 'MinecraftTen';");
 		} else {
 			document.getElementById("articleTitle").innerText = "Version not found!";
 			document.getElementById("articleBody").innerHTML = "This version is not found, try searching for a different version.";
