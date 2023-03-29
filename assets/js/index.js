e110292ce292823e2cd6cb1a44360b7d3bca60ef
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-03-29 18:50:18 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,21 @@
 const params = window.location.search.replace("?", "").split("&").map((i) => ({ name: i.split("=")[0], value: i.split("=")[1] }));
 const paths = window.location.pathname.split("/");
-const articles = fetch("./data/preview-articles.json").then((response) => response.json());
+(
+	async () => {
+		const articles = await fetch("./data/preview-articles.json").then((response) => response.json());
 
-const article = articles.find((a) => a.version == params.find((p) => p.name == "version")?.value);
-if (article) {
-	document.getElementById("articleTitle").innerText = article.article.title;
-	document.getElementById("articleBody").innerHTML = article.article.body;
-} else {
-	document.getElementById("articleTitle").innerText = "Version not found!";
-	document.getElementById("articleBody").innerHTML = "This version is not found, try searching for a different version.";
-	document.getElementById("articleBody").style = "text-align: center;";
-};
+		const article = articles.find((a) => a.version == params.find((p) => p.name == "version")?.value);
+		if (article) {
+			document.getElementById("articleTitle").innerText = article.article.title;
+			document.getElementById("articleBody").innerHTML = article.article.body;
+		} else {
+			document.getElementById("articleTitle").innerText = "Version not found!";
+			document.getElementById("articleBody").innerHTML = "This version is not found, try searching for a different version.";
+			document.getElementById("articleBody").style = "text-align: center;";
+		};
 
-console.log("Version:", article?.version || "???");
-console.log("Beta:", params.find((p) => p.name == "beta")?.value || "false" == true);
-console.log(article);
+		console.log("Version:", article?.version || "???");
+		console.log("Beta:", params.find((p) => p.name == "beta")?.value || "false" == true);
+		console.log(article);
+	}
+)();
